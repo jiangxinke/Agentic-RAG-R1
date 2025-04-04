be6df847673c722e2b02b9ee4a9b43ca6ebef11e
Xinke Jiang <1642554832@qq.com>
2025-04-04 23:22:52 +0800

    change gitignore

--- script/train_zero2.sh
+++ script/train_zero2.sh
@@ -4,9 +4,9 @@ export NCCL_P2P_LEVEL=NVL
 export ELASTIC_SEARCH_URL="https://123.57.228.132:8285"
 export ELASTIC_SEARCH_PASSWORD="Qvw_toYzUTZkwmYaT5bC"
 
-CUDA_VISIBLE_DEVICES=2 accelerate launch \
+CUDA_VISIBLE_DEVICES=6,7 accelerate launch \
     --config_file ./config/accelerate_train_zero2_config.yaml \
     --main_process_port 12347 \
-    --num_processes 1 \
+    --num_processes 2 \
     --mixed_precision "bf16" \
     train.py
