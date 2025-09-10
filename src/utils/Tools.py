b73409cfdd75a67f8e62959c710098407fcf6abc
jiangxinke <1642554832@qq.com>
2025-09-10 10:53:02 +0800

    add mock tools

--- src/utils/Tools.py
+++ src/utils/Tools.py
@@ -159,9 +159,11 @@ class Tools:
     def Wiki_RAG(self, input: str) -> str:
         logging.info(f"Using Wiki_RAG input: {input}")
         # wiki_searcher = create_wiki_searcher("zh")
-        wiki_searcher = create_wiki_searcher("en")
-        assert wiki_searcher is not None
-        RAG_result = wiki_searcher.search(input)
+        # FIXME 先暂时mock一个
+        # wiki_searcher = create_wiki_searcher("en")
+        # assert wiki_searcher is not None
+        # RAG_result = wiki_searcher.search(input)
+        RAG_result = "暂时无法使用工具，请不要使用"
         RAG_result = RAG_result if RAG_result else "无法检索到医学知识，请规范用户输入"
         logging.info(f"Wiki_RAG result: {str(RAG_result)[:200]}")
         return str(RAG_result)[:200]
